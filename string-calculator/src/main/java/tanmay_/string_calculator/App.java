2008a101b4da4162a5856bd0adc4fe792351e191
Tanmay_C <Tanmay_C@DESKTOP-72IBUN9>
2021-02-27 23:39:37 +0530

    Created Project

---
+++ string-calculator/src/main/java/tanmay_/string_calculator/App.java
@@ -0,0 +1,13 @@
+package tanmay_.string_calculator;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
