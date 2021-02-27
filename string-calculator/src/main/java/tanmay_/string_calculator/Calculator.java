bfd6df5a66063735260fe74b0b5ae6694875b064
Tanmay_C <Tanmay_C@DESKTOP-72IBUN9>
2021-02-28 00:49:56 +0530

    case : single number returns value

--- string-calculator/src/main/java/tanmay_/string_calculator/Calculator.java
+++ string-calculator/src/main/java/tanmay_/string_calculator/Calculator.java
@@ -4,7 +4,11 @@ package tanmay_.string_calculator;
 public class Calculator {
 
 	public int calculate(String string) {
+		if(string.isEmpty()) {
 		return 0;
+		}
+		return Integer.parseInt(string);
 	}
+	
 
 }
--- string-calculator/src/main/java/tanmay_/string_calculator/TestCalculator.java
+++ string-calculator/src/main/java/tanmay_/string_calculator/TestCalculator.java
@@ -1,14 +1,25 @@
 package tanmay_.string_calculator;
 
 
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 import static org.testng.Assert.assertEquals;
 
 @Test
 public class TestCalculator {
+	
+	private Calculator calculator;
+	
+	@BeforeTest
+	public void init() {
+		calculator = new Calculator();
+		
+	}
 	public void emptyStringReturnsZero() {
-		Calculator calculator = new Calculator();
 		assertEquals(calculator.calculate(""), 0);
+	}
+	public void singleValueReturned() {
+		assertEquals(calculator.calculate("1"), 1);
 	}
 
 }
